ca2f548d5aa888fabda7dee83a314a353035f103
Martina <martina.rossini704@gmail.com>
2021-05-19 13:05:35 +0200

    Fixed bbox limits and adjusted semiaxes

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ import matplotlib.image as mpimg
 import matplotlib.patches as patches
 
 #Globals
-IMG_SIZE = 200
+IMG_SIZE = 128
 IMG_PATH = './data/SKAMid_B1_1000h_v3.fits'
 TRAINING_SET_PATH = './training_set/TrainingSet_B1_v2.txt'
 
@@ -64,6 +64,22 @@ def deb_plot(x, y):
 
 	return
 
+def adjustSemiAxis(major,minor,category,size,BMAJ_RATIO,BMIN_RATIO,G_SQUARED):
+
+	if category == 1 and size == 1:
+		bmaj, bmin = major/2.0,minor/2.0
+	elif category == 2 and size == 2:
+		bmaj, bmin = major/2.0,major/2.0
+	elif category == 3 and size == 3:
+		bmaj, bmin = major*(2**0.5),minor*(2**0.5)
+	else:
+		bmaj, bmin =  major,minor
+	
+	major = np.sqrt(bmaj ** 2 + G_SQUARED) * BMAJ_RATIO
+	minor = np.sqrt(bmin ** 2 + G_SQUARED) * BMIN_RATIO
+
+	return major, minor
+
 #For now filters with the threshold but should works with everything I hope
 def newDivideImages():
 
@@ -80,7 +96,13 @@ def newDivideImages():
 	TrainingSet['y'] = TrainingSet['y'].astype(int)
 	TrainingSet['FLUX'] = TrainingSet['FLUX'].astype(float)
 
+	#Constants
 	X_PIXEL_RES = abs(fits_img.header['CDELT1'])
+	BMAJ_TOT = abs(fits_img.header['BMAJ'])
+	BMIN_TOT = abs(fits_img.header['BMIN'])
+	BMAJ_RATIO = BMAJ_TOT / X_PIXEL_RES
+	BMIN_RATIO = BMIN_TOT / X_PIXEL_RES
+	G_SQARED = (2 * X_PIXEL_RES * 3600) ** 2
 	WORLD_REF = pywcs.WCS(fits_img.header).deepcopy()
 
 	fits_img = fits_img.data[0,0]
@@ -88,7 +110,7 @@ def newDivideImages():
 	for i in range(16300,32768,IMG_SIZE):
 		for j in range(16300,32768,IMG_SIZE):
 			#Query
-			small_ts = TrainingSet.query('x < @i+@IMG_SIZE and x >= @i and y < @j+@IMG_SIZE and y >= @j and FLUX > @FLUX_TRESHOLD')
+			small_ts = TrainingSet.query('x < @i+@IMG_SIZE and x >= @i and y < @j+@IMG_SIZE and y >= @j and FLUX > @FLUX_TRESHOLD and SELECTION == 1')
 			#Not-empty section
 			if (len(small_ts)) > 0:
 				pos = (i+(IMG_SIZE/2), j+(IMG_SIZE/2))
@@ -100,11 +122,11 @@ def newDivideImages():
 					if row['FLUX'] > FLUX_TRESHOLD:
 
 						#Print Debugging
-						#flux = row['FLUX']
-						#print('x: ', str(row['x']), "y: ", str(row['y']), " Flux =", flux)
-						#print("Normal i and j :",i, "  ", j)
-						#print(str(i+IMG_SIZE), "  ", str(j+IMG_SIZE))
-						
+						flux = row['FLUX']
+						print('x: ', str(row['x']), "y: ", str(row['y']), " Flux =", flux)
+						print("Normal i and j :",i, "  ", j)
+						print(str(i+IMG_SIZE), "  ", str(j+IMG_SIZE))
+						print("----------------------------")
 						#Center
 						centroid_x = int(row['x']-i)
 						centroid_y = int(row['y']-j)
@@ -114,86 +136,42 @@ def newDivideImages():
 						major = (row['BMAJ'] / 3600 / X_PIXEL_RES) / 2
 						minor = (row['BMIN'] / 3600 / X_PIXEL_RES) / 2
 						phi = np.radians(row['PA'])
+
+						major,minor = adjustSemiAxis(major,minor,row.CLASS,row.SIZE, BMAJ_RATIO, BMIN_RATIO, G_SQARED)
+
+						#Fixed 
 						xmin, ymin, xmax, ymax = ellipse_to_box(phi, major, minor, centroid_x, centroid_y)
+						xmin = max(xmin, 0)
+						ymin = max(ymin, 0)
+						xmax = min(xmax,IMG_SIZE-1)
+						ymax = min(ymax,IMG_SIZE-1)
+
+						if row.CLASS == 1:
+							color = 'yellow'
+							text = '1: SS-AGN'
+						elif row.CLASS == 2:
+							color = 'green'
+							text = '2: FS-AGN'
+						else:
+							color = 'blue'
+							text = '3: SFG'
 						box = patches.Rectangle((xmin, ymin),
                                                     width=xmax - xmin,
                                                     height=ymax - ymin,
                                                     linewidth=1,
-                                                    edgecolor='y',
+                                                    edgecolor=color,
                                                     facecolor='none')
 						#Update plot
 						ax.add_patch(center)
 						ax.add_patch(box)
-						ax.imshow(img_array,cmap='gist_heat')
+						ax.imshow(img_array,cmap='gist_heat', origin='lower')
+						ax.text(xmin,ymin,text,c=color)
+						ax.text(xmax, ymax, 'XMAX', c=color)
 						
 						#Debugging
 						#deb_plot(row['x'],row['y'])
 				plt.show()
 	return
-
-def divideImages():
-	# Divide the fits image in IMG_SIZE X IMG_SIZE images
-	fits_img = fits.open(IMG_PATH)
-	fits_img = make_fits_2D(fits_img[0])
-	TrainingSet=pd.read_csv(TRAINING_SET_PATH,skiprows=17,delimiter='\s+')
-	TrainingSet=TrainingSet[TrainingSet.columns[0:15]]
-	TrainingSet.columns=['ID','RA (core)','DEC (core)','RA (centroid)','DEC (centroid)','FLUX','Core frac','BMAJ','BMIN','PA','SIZE','CLASS','SELECTION','x','y']
-	TrainingSet['x'] = TrainingSet['x'].astype(int)
-	TrainingSet['y'] = TrainingSet['y'].astype(int)
-
-	X_PIXEL_RES = abs(fits_img.header['CDELT1'])
-	WORLD_REF = pywcs.WCS(fits_img.header).deepcopy()
-
-	fits_img=fits_img.data[0,0]
-
-	#img_array = np.empty((0,IMG_SIZE,IMG_SIZE))
-
-	for i in range(16300,32768,IMG_SIZE):
-		for j in range(16300,32768,IMG_SIZE):
-			#img_array = fits_img[i:i+IMG_SIZE,j:j+IMG_SIZE]
-			pos = (i + IMG_SIZE/2, j + IMG_SIZE/2)
-			img_fits = Cutout2D(fits_img, position=pos, size=IMG_SIZE, wcs=WORLD_REF, copy=True)
-			img_array = img_fits.data
-			small_ts = TrainingSet.query('x < @i+@IMG_SIZE and x >= @i and y < @j+@IMG_SIZE and y >= @j')
-			if (len(small_ts)) > 0:
-				print('CIAO')
-				#hdu = fits.PrimaryHDU(img_array, header = img_fits.wcs.to_header())
-				#hdul = fits.HDUList([hdu])
-				#hdul.writeto('new_image.fits')
-				_, ax = plt.subplots()
-				ax.imshow(img_array, cmap='gist_heat')
-				for _, row in small_ts.iterrows():
-					major = (row['BMAJ'] / 3600 / X_PIXEL_RES ) / 2
-					minor = (row['BMIN'] / 3600 / X_PIXEL_RES ) / 2
-					phi = np.radians(row['PA'])
-					x = row['x'] % IMG_SIZE
-					y = row['y'] % IMG_SIZE
-					print('y: ', y, ' x: ', x)
-					#ra_min, dec_min, ra_max, dec_max = ellipse_to_box(phi, major, minor, row['RA (centroid)'], row['DEC (centroid)'])
-					#xmin, ymin = WORLD_REF.wcs_world2pix([[ra_min, dec_min]], 0)[0]
-					#xmax, ymax = WORLD_REF.wcs_world2pix([[ra_max, dec_max]], 0)[0]
-					xmin, ymin, xmax, ymax = ellipse_to_box(phi, major, minor, x, y)
-					center = patches.Circle((x, y), radius=1, edgecolor='g', facecolor='none')
-					#xmin, xmax = xmin -i, xmax -i
-					#ymin, ymax = ymin -j, ymax -j 
-					box = patches.Rectangle((xmin, ymin), 
-											width=xmax - xmin, 
-											height=ymax - ymin,
-											linewidth=1,
-											edgecolor='y',
-											facecolor='none')
-					ax.add_patch(box)
-					ax.add_patch(center)			
-				plt.show()
-			#np.save("../data/training/B1_1000h/img" + str(i) + str(j) + ".npy", img_array)
-			#small_ts.to_csv('../data/training/csv/img' + str(i) + str(j) + ".csv", header=None)
-			#print("I: ",i," ",i+IMG_SIZE)
-			#print("J: ",j," ",j+IMG_SIZE)
-			#print(small_ts[['ID','x','y']])
-			#if (len(small_ts)) >0:
-				#plt.imshow(img_array,cmap='gist_heat')
-				#plt.show()
-				#return
 			
 def ellipse_to_box(phi, major, minor, x, y):
 	axis_ux = major * np.cos(phi)
