2106a42bd3af26caf207cdd89a48ac236313fb91
Martina <martina.rossini704@gmail.com>
2021-06-11 13:15:56 +0200

    Adjusted config file

--- src/configs/yolo_v4.py
+++ src/configs/yolo_v4.py
@@ -9,6 +9,7 @@ List is structured by "B" indicating a residual block followed by the number of 
 
 NUM_CLASSES = 3
 
+#Backbone
 cspdarknet53 = [
     (32, 3, 1, 'same', 'mish'),
     ["CSP", 1],
@@ -23,30 +24,33 @@ cspdarknet53 = [
     (1024, 1, 1, 'same', 'mish'),
 ]
 
-neck = [
-    (512, 1, 1, 'same', 'leaky'),# input_3
-    (1024, 3, 1, 'same', 'leaky'),
-    ["M"],
+#Neck
+panet = [
+    'S', # output small
     (512, 1, 1, 'same', 'leaky'),
     (1024, 3, 1, 'same', 'leaky'),
-    (512, 1, 1, 'same', 'leaky'),# output_3
-    (256, 1, 1, 'same', 'leaky'),
-    "U",
-    ["C"],
+    (512, 1, 1, 'same', 'leaky'),
+    ["SPP"],
+    (512, 1, 1, 'same', 'leaky'),
+    (1024, 3, 1, 'same', 'leaky'),
+    (512, 1, 1, 'same', 'leaky'),
+    ["U", 256],
+    'M', # output medium
+    ["C", 256, 1, 1, 'same', 'leaky'],
     (256, 1, 1, 'same', 'mish'),
     (512, 3, 1, 'same', 'mish'),
     (256, 1, 1, 'same', 'mish'),
     (512, 3, 1, 'same', 'mish'),
-    (256, 1, 1, 'same', 'mish'),# output_2
-    (128, 1, 1, 'same', 'leaky'),
-    "U",
-    ["C"],
+    (256, 1, 1, 'same', 'mish'),
+    ["U", 128],
+    'L', # output large
+    ["C", 128, 1, 1, 'same', 'leaky'],
     (128, 1, 1, 'same', 'leaky'),
     (256, 3, 1, 'same', 'mish'),
     (128, 1, 1, 'same', 'leaky'),
     (256, 3, 1, 'same', 'mish'),
     (128, 1, 1, 'same', 'leaky'),
-    (256, 3, 1, 'same', 'mish'),# output_1
+    ['A', 3]
 ]
 
 head = []
