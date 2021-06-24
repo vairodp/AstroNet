e980df4fbd1d329e0b21c09f406ece8d41826a7c
Martina <martina.rossini704@gmail.com>
2021-06-24 13:10:12 +0200

    Added anchor boxes and finished compute_loss method

--- src/loss.py
+++ src/loss.py
@@ -4,10 +4,11 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.losses import Loss, binary_crossentropy
 
-from configs.yolo_v4 import NUM_CLASSES, loss_params
+from configs.yolo_v4 import ANCHORS, ANCHORS_MASKS, NUM_CLASSES, loss_params
 
 class YoloLoss(Loss):
     def __init__(self, 
+                iou_threshold,
                 smooth_factor=0.0, 
                 use_giou=False, 
                 use_ciou=False, 
@@ -15,15 +16,17 @@ class YoloLoss(Loss):
                 **kwargs):
         super().__init__(**kwargs)
         self.smooth_factor = smooth_factor
+        self.iou_threshold = iou_threshold
         self.use_giou = use_giou
         self.use_ciou = use_ciou
         self.use_diou = use_diou
-        self.anchors = '' #TODO understand how to create the anchors
+        self.anchors = ANCHORS
+        self.anchors_masks = ANCHORS_MASKS
 
     def label_smoothing(self, true_labels):
         return true_labels * (1.0 - self.smooth_factor) + self.smooth_factor / NUM_CLASSES
         
-    def interpret_prediction_boxes(self, pred):
+    def interpret_prediction_boxes(self, pred, anchors):
         grid_size = tf.shape(pred)[1]
         box_xy, box_wh, objectness, probs = tf.split(pred, (2, 2, 1, -1), axis=-1)
 
@@ -36,7 +39,7 @@ class YoloLoss(Loss):
         grid = tf.expand_dims(tf.stack(grid, axis=-1), axis=2)
 
         box_xy = (box_xy + tf.cast(grid, tf.float32)) / tf.cast(grid_size, tf.float32)
-        box_wh = tf.exp(box_wh) * self.anchors
+        box_wh = tf.exp(box_wh) * anchors
 
         box_x1y1 = box_xy - box_wh / 2
         box_x2y2 = box_xy + box_wh / 2
@@ -52,7 +55,7 @@ class YoloLoss(Loss):
         
         return true_xy, true_wh, true_obj, true_class
 
-    def interpret_true_boxes(self, y_true):
+    def interpret_true_boxes(self, y_true, anchors):
         grid_size = tf.shape(y_true)[1]
         grid = tf.meshgrid(tf.range(grid_size), tf.range(grid_size))
         grid = tf.expand_dims(tf.stack(grid, axis=-1), axis=2)
@@ -60,7 +63,7 @@ class YoloLoss(Loss):
         true_xy, true_wh, _, _ = self.get_true_scores(y_true)
         true_xy = true_xy * tf.cast(grid_size, tf.float32) - \
                       tf.cast(grid, tf.float32)
-        true_wh = tf.math.log(true_wh / self.anchors)
+        true_wh = tf.math.log(true_wh / anchors)
         true_wh = tf.where(tf.math.is_inf(true_wh),
                                tf.zeros_like(true_wh), true_wh)
 
@@ -84,10 +87,10 @@ class YoloLoss(Loss):
     
     def broadcast_iou(self, box_true, box_pred):
         # broadcast boxes
-        box_true = tf.expand_dims(box_true, -2)
-        box_pred = tf.expand_dims(box_pred, 0)
+        box_pred = tf.expand_dims(box_pred, -2)
+        box_true = tf.expand_dims(box_true, 0)
         # new_shape: (..., N, (x1, y1, x2, y2))
-        new_shape = tf.broadcast_dynamic_shape(tf.shape(box_true), tf.shape(box_pred))
+        new_shape = tf.broadcast_dynamic_shape(tf.shape(box_pred), tf.shape(box_true))
         box_true = tf.broadcast_to(box_true, new_shape)
         box_pred = tf.broadcast_to(box_pred, new_shape)
 
@@ -148,8 +151,8 @@ class YoloLoss(Loss):
         # https://www.analyticsvidhya.com/blog/2020/08/a-beginners-guide-to-focal-loss-in-object-detection/
         pass
 
-    def compute_loss(self, y_true, y_pred):
-        box_pred, obj_pred, class_pred, raw_box_pred = self.interpret_prediction_boxes(y_pred)
+    def compute_loss(self, y_true, y_pred, anchors):
+        box_pred, obj_pred, class_pred, raw_box_pred = self.interpret_prediction_boxes(y_pred, anchors)
 
         xy_true, wh_true, obj_true, class_true = self.get_true_scores(y_pred)
         box_true = tf.concat([xy_true - wh_true/2.0, xy_true + wh_true/2.0], axis=-1)
@@ -160,5 +163,75 @@ class YoloLoss(Loss):
         # in order to element-wise multiply the result from tf.reduce_sum
         # we need to squeeze one dimension for objectness here
         obj_mask = tf.squeeze(obj_true, axis=-1)
+        filtered_box_true = tf.boolean_mask(box_true, tf.cast(obj_mask, tf.bool))
+        best_iou, _ = tf.map_fn(lambda x: (tf.reduce_max(self.broadcast_iou(x[0], x[1]), axis=-1), 0), 
+                                (filtered_box_true, box_pred))
 
-        #TODO: Finish method
+        ignore_mask = tf.cast(best_iou < self.iou_threshold, tf.float32)
+
+        '''
+        if self.use_focal_obj_loss:
+            confidence_loss = self.focal_loss(true_obj, pred_obj)
+        else:
+        '''
+        confidence_loss = binary_crossentropy(obj_true, obj_pred)
+        confidence_loss = obj_mask * confidence_loss + (1 - obj_mask) * ignore_mask * confidence_loss
+
+        """
+        if self.use_focal_loss:
+            class_loss = self.focal_loss(true_class, pred_class)
+        else:
+        """
+        class_loss = obj_mask * binary_crossentropy(class_true, class_pred)
+
+        # box loss
+        if self.use_giou:
+            giou = self.giou(box_true, box_pred)
+            box_loss = obj_mask * weights * (1 - giou)
+            box_loss = tf.reduce_sum(box_loss, axis=(1, 2, 3))
+        elif self.use_ciou:
+            ciou = self.ciou(box_true, box_pred)
+            box_loss = obj_mask * weights * (1 - ciou)
+            box_loss = tf.reduce_sum(box_loss, axis=(1, 2, 3))
+        elif self.use_diou:
+            diou = self.diou(box_true, box_pred)
+            box_loss = obj_mask * weights * (1 - diou)
+            box_loss = tf.reduce_sum(box_loss, axis=(1, 2, 3))
+        else:
+            # traditional loss for xy and wh
+            pred_xy = raw_box_pred[..., 0:2]
+            pred_wh = raw_box_pred[..., 2:4]
+
+            # invert box equation
+            true_xy, true_wh = self.interpret_true_boxes(y_true, anchors)
+
+            # sum squared box loss
+            xy_loss = obj_mask * weights * \
+                      tf.reduce_sum(tf.square(true_xy - pred_xy), axis=-1)
+            wh_loss = obj_mask * weights * \
+                      tf.reduce_sum(tf.square(true_wh - pred_wh), axis=-1)
+
+            xy_loss = tf.reduce_sum(xy_loss, axis=(1, 2, 3))
+            wh_loss = tf.reduce_sum(wh_loss, axis=(1, 2, 3))
+            box_loss = xy_loss + wh_loss
+
+        # sum of all loss
+        confidence_loss = tf.reduce_sum(confidence_loss, axis=(1, 2, 3))
+        class_loss = tf.reduce_sum(class_loss, axis=(1, 2, 3))
+
+        return box_loss + confidence_loss + class_loss
+
+    def call(self, y_true, y_pred):
+        true_small, true_med, true_large = y_true
+        pred_small, pred_med, pred_large = y_pred
+
+        # Small bbox loss
+        loss_small = self.compute_loss(true_small, pred_small, self.anchors[self.anchors_masks[0]])
+
+        # Medium bbox loss
+        loss_med = self.compute_loss(true_med, pred_med, self.anchors[self.anchors_masks[1]])
+        
+        # Large bbox loss
+        loss_large = self.compute_loss(true_large, pred_large, self.anchors[self.anchors_masks[2]])
+
+        return tf.reduce_sum(loss_small + loss_med + loss_large)
